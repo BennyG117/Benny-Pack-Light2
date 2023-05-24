d9573fb1d132b12de1a920b5d060a8953c047ab6
BennyG117 <bengomezp@gmail.com>
2023-05-24 10:21:09 -0700

    Initial Commit Benny Pack Light 2

---
+++ script.js
@@ -0,0 +1,38 @@
+function tripFocus(element) {
+    //when trip is selected change innerText of "itemSheet" to corresponding packing list
+    //! Hiking (Cold)
+    //if Hiking (Cold) then change innerText of "itemSheet";
+    // log packList1
+
+    //! Fancy Trip
+    //if Fancy Trip then change innerText of "itemSheet";
+    // log packList2
+
+    //! Hiking (Hot)
+    //if Hiking (Hot) then change innerText of "itemSheet";
+    // log packList3
+
+    //! Beach Trip
+    //if Beach Trip then change innerText of "itemSheet";
+    // log packList4
+
+    //! Lake Trip
+    //if Lake Trip then change innerText of "itemSheet";
+    // log packList5
+
+    //! Museum Trip
+    //if Museum Trip then change innerText of "itemSheet";
+    // log packList6
+
+    //! Nerd Trip
+    //if Nerd Trip then change innerText of "itemSheet";
+    // log packList7
+
+    //! Ski Trip
+    //if Ski Trip then change innerText of "itemSheet";
+    // log packList8
+
+    //! Hiking (Rain)
+    //if Hiking (Rain) then change innerText of "itemSheet";
+    // log packList9
+}
