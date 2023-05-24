8c4c14b579d84c96d008420564e4241c02b26b35
BennyG117 <bengomezp@gmail.com>
2023-05-24 16:18:08 -0700

    BPL2 update

--- script.js
+++ script.js
@@ -1,4 +1,136 @@
-function tripFocus(element) {
+function tripSelector(element) {
+    console.log(element.value);
+    if (element.value == "hikingCold") {
+        let variable = document.querySelector("#itemSheet")
+        console.log(variable);
+            console.log(variable.innerHTML);
+            variable.innerHTML = `<li>banana</li>`
+    }
+
+
+//hikingCold List:
+/* <li>T-Shirt x </li>
+<li>Exercise Shirt x </li>
+<li>Polo x </li>
+<li>Long-Slvd Shirt x </li>
+<li>Sun-Shirt x </li>
+<li>Dress Shirt x </li>
+<li>Under Shirt x </li>
+<li>Pajama Shirt x </li>
+<li>Jacket x </li>
+<li>Rain Jacket x </li>
+<li>Suit Jacket x </li>
+<li>Ski Jacket x </li>
+<li>Tie x </li>
+<li>Mask x </li>
+<li>Bandana x </li>
+<li>Head Band x </li>
+<li>Regular Belt x</li>
+<li>Dress Belt x </li>
+<li>Hiking Belt x </li>
+<li>Shorts x </li>
+<li>Gym Shorts x </li>
+<li>Swimsuit x </li>
+<li>Hiking Pants x</li>
+<li>Jeans x </li>
+<li>Dress Pants x</li>
+<li>Pajama Pants x </li>
+<li>Pajama Shorts x</li>
+<li>Waterproof Pants x </li>
+<li>Ski Pants x </li>
+<li>Briefs x </li>
+<li>Winter Undrwear x </li>
+<li>Short Socks x </li>
+<li>Long Socks x </li>
+<li>Winter Socks x </li>
+<li>Snapback x </li>
+<li>Trail Hat x </li>
+<li>Beanie x </li>
+<li>Snow Gloves x </li>
+<li>Reg Gloves x </li>
+<li>Scarf x </li>
+<li>Sneakers x </li>
+<li>Waterproof Boots x </li>
+<li>Reg Hiking Boots x </li>
+<li>Dress Shoes x </li>
+<li>Water Shoes x </li>
+<li>Sandals x</li>
+<li>Reg-Glasses x </li>
+<li>Sunglasses x </li>
+<li>Lense Cleaner x </li>
+<li>Eye Drops x </li>
+<li>Toothpaste x </li>
+<li>Toothbrush x </li>
+<li>Floss Sticks x </li>
+<li>Meds x </li>
+<li>Inhaler x </li>
+<li>vitamins x </li>
+<li>Sunscreen x </li>
+<li>Lotion x </li>
+<li>Hand Sanitizer x </li>
+<li>Clorox Trvl x </li>
+<li>Body Wash x </li>
+<li>Shampoo x </li>
+<li>Face Cleaner x </li>
+<li>Chapstick x </li>
+<li>Comb x</li>
+<li>Hair Putty x</li>
+<li>Deodorant x </li>
+<li>Shaving Kit x </li>
+<li>Tissues x </li>
+<li>Lint Roller x </li>
+<li>Shoe Shine x</li>
+<li>Bug Spray x </li>
+<li>Talcum Powder x </li>
+<li>Misc BR Items x </li>
+<li>Travel Towel x </li>
+<li>Travel Pillow x</li>
+<li>Hiking Bag x </li>
+<li>Sling Bag x </li>
+<li>Laundry Bag x</li>
+<li>Rain Covers x </li>
+<li>Laptop & gear x</li>
+<li>Phone Charger x </li>
+<li>Charge Cord x </li>
+<li>Travel Btry Sm x </li>
+<li>Travel Btry Lrg x </li>
+<li>Wireless Buds x</li>
+<li>Switch & gear x </li>
+<li>Extension Cord x </li>
+<li>Gum x </li>
+<li>Snacks x </li>
+<li>Empty Bottle x </li>
+<li>Lifting Wraps x </li>
+<li>Wallet x </li>
+<li>Keys x </li>
+<li>Passport x </li>
+<li>Silicone Ring x </li>
+<li>Ring x </li>
+<li>Ring Bandit x </li>
+<li>Emgcy Cash x </li>
+<li>Notepad & Pen x </li>
+<li>Sharpie x </li>
+<li>H2O Proof Pouch x </li>
+<li>Rmv Swiss Knife x</li>
+<li>First Aid Kit x</li>
+<li>MTG Cards x </li>
+<li>MTG Playmat x </li>
+<li>Counters & Dice x </li>
+*/
+
+
+
+
+
+
+
+
+
+
+
+    // if (    )...... if.... else...
+
+
     //when trip is selected change innerText of "itemSheet" to corresponding packing list
     //! Hiking (Cold)
     //if Hiking (Cold) then change innerText of "itemSheet";
@@ -35,4 +167,9 @@ function tripFocus(element) {
     //! Hiking (Rain)
     //if Hiking (Rain) then change innerText of "itemSheet";
     // log packList9
+
+
+    //?doc query selector**
 }
+// onchange select, change inner text/ 1 function with a bunch of else if statements
+
